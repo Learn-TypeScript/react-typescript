c6fd6ec167ca7fd46143cdbe0dfce4607a80abb3
fotis tsakiris <footios76@gmail.com>
2020-10-13 05:14:28 +0300

    add new todos on the list

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,16 +1,17 @@
 import React, { useRef } from 'react';
 
 export interface NewToDoProps {
-
+    onAddTodo: (text: string) => void;
 }
 
-const NewToDo: React.SFC<NewToDoProps> = () => {
+const NewToDo: React.SFC<NewToDoProps> = (props) => {
     const textInputRef = useRef<HTMLInputElement>(null);
 
     const todoSubmitHandler = (event: React.FormEvent) => {
         event.preventDefault();
         const enteredText = textInputRef.current!.value;
         console.log(enteredText);
+        props.onAddTodo(enteredText)
 
     }
     return (
